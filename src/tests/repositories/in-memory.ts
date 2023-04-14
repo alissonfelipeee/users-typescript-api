a774630990272948c6096896fad24fe466f6c7e1
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-04-14 18:41:24 -0300

    feat: add jest tests update user

--- src/tests/repositories/in-memory.ts
+++ src/tests/repositories/in-memory.ts
@@ -1,3 +1,4 @@
+import { UpdateUserParams } from "../../controllers/update-user/protocols";
 import { User } from "../../models/user";
 
 export class InMemoryUserRepository {
@@ -10,5 +11,13 @@ export class InMemoryUserRepository {
   async createUser(user: User): Promise<User> {
     this.users.push(user);
     return user;
+  }
+
+  async updateUser(id: number, params: UpdateUserParams): Promise<User> {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    const user = this.users[userIndex];
+    const updatedUser = { ...user, ...params };
+    this.users[userIndex] = updatedUser;
+    return updatedUser;
   }
 }
---
+++ src/tests/updateUser.test.ts
@@ -0,0 +1,90 @@
+import { UpdateUserController } from "./../controllers/update-user/update-user";
+import { InMemoryUserRepository } from "./repositories/in-memory";
+
+const userExample = {
+  id: 1,
+  firstName: "John",
+  lastName: "Doe",
+  email: "johndoe@gmail.com",
+  password: "123456",
+};
+
+describe("Update User", () => {
+  it("should update user", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    const userCreated = await inMemoryUserRepository.createUser(userExample);
+
+    const user = await updateUserController.handle({
+      params: {
+        id: userCreated.id,
+      },
+      body: {
+        lastName: "Doe Jr",
+      },
+    });
+
+    expect(user.body).toEqual({
+      ...userCreated,
+      lastName: "Doe Jr",
+    });
+  });
+
+  it("should return 400 if id is not provided", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    const user = await updateUserController.handle({
+      params: {},
+      body: {
+        lastName: "Doe Jr",
+      },
+    });
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body).toBe("Bad Request - Missing id");
+  });
+
+  it("should return 400 if there are invalid fields", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    const user = await updateUserController.handle({
+      params: {
+        id: 1,
+      },
+      body: {
+        email: "johndoe2@gmail.com",
+      },
+    });
+
+    expect(user.statusCode).toBe(400);
+    expect(user.body).toBe("Bad Request - Invalid fields");
+  });
+
+  it("should return 404 if user not found by id", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    const user = await updateUserController.handle({
+      params: {
+        id: 2,
+      },
+      body: {
+        lastName: "Doe Jr",
+      },
+    });
+
+    expect(user.statusCode).toBe(404);
+    expect(user.body).toBe("Not Found - User not found");
+  });
+});
