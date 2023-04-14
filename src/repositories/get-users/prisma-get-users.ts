2c2b7a24328fdef43aface36950a777a6cc2476d
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-04-14 10:01:25 -0300

    feat: ensure GetUsersPrismaRepository gets users from prisma

--- src/repositories/get-users/prisma-get-users.ts
+++ src/repositories/get-users/prisma-get-users.ts
@@ -1,15 +1,10 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { prisma } from "../../database/prisma";
 import { User } from "../../models/user";
 
 export class PrismaGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "John",
-        lastName: "Doe",
-        email: "johndoe@gmail.com",
-        password: "123456",
-      },
-    ];
+    const users = await prisma.user.findMany() as User[];
+    return users;
   }
 }
